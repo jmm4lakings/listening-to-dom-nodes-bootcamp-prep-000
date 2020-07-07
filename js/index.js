46a027a11d8552db89b90ca8ba87ee45ec2c5a92
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-07 19:59:07 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+const input = document.querySelector('input');
+ 
+input.addEventListener('keydown', function(e) {
+  console.log(e.key);
+});
